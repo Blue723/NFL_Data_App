8cb64a74ef9d29bd85109711e09330afd666fc6f
Morgan Keith <morgan.a.keith92@gmail.com>
2024-07-17 19:22:38 -0500

    Streamlit Other Webpages

--- pages/.ipynb_checkpoints/1_Teams-checkpoint.py
+++ pages/.ipynb_checkpoints/1_Teams-checkpoint.py
@@ -63,8 +63,9 @@ tables = {
     'Opponent Touchdown Log': 'opponent_touchdown_log'
 }
 
+#table names
 table_title = list(tables.keys())
-
+#table file names
 table_file = list(tables.values())
 
 #get dataframe
@@ -80,18 +81,28 @@ def team_schedule (year:int, team:str):
     schedule_and_game_results = pd.read_csv(file, index_col=0)
     
     query = f'''
-    select Week, Opponent, Team, Year
-    from schedule_and_game_results
-    where Team='{team}' and Year={year}
-    order by Week
+        select Week, Opponent, Team, Year
+        from schedule_and_game_results
+        where Team='{team}' and Year={year}
+        order by Week
     '''
 
     df = duckdb.sql(query).df()
 
     return df
 
+#get opponent
+def get_opponent(year, week, team):
+    file = 'Football_Data/schedule_and_game_results'
+    
+    schedule_and_game_results = pd.read_csv(file, index_col=0)
+
+    str_opp_team = schedule_and_game_results[(schedule_and_game_results['Year']==year) & (schedule_and_game_results['Week'] == week) & (schedule_and_game_results['Team']==team)]['Opponent'].iloc[0]
+
+    return str_opp_team
+
 #get opponent table
-def query_df(schedule_df, year:int, team:str, table:str, week:int):
+def opp_table_df(schedule_df, year:int, team:str, table:str, week:int):
     
     opp_filter = schedule_df[schedule_df['Week'] == week]
     
@@ -101,9 +112,9 @@ def query_df(schedule_df, year:int, team:str, table:str, week:int):
     table_df = pd.read_csv(file, index_col=0)
 
     table_query = f'''
-    select *
-    from table_df
-    where (Team='{team}' or Team='{str_opp_team}') and Year={year}
+        select *
+        from table_df
+        where (Team='{team}' or Team='{str_opp_team}') and (Year={year})
     '''
 
     df = duckdb.sql(table_query).df()
@@ -117,13 +128,17 @@ def df_year (year: int, table):
     file = f'Football_Data/{table}'
     table_df = pd.read_csv(file, index_col=0)
     
-    query = f'''select * from table_df where Year = {year}'''
+    query = f'''
+        select * 
+        from table_df 
+        where Year = {year}
+    '''
     
     df = duckdb.sql(query).df()
 
     return df
 
-#bar chart
+#bar chart for tables
 def px_bar_charts(df: pd, team, column):
     try:
         fig1 = px.bar(
@@ -150,37 +165,96 @@ def px_bar_charts(df: pd, team, column):
     return st.plotly_chart(fig1)
 
 
-#piechart
+#piechart for tables
 def px_pie_charts(df, team, column):
     fig1 = px.pie(data_frame=df, values=df[column], names=df['Player'], title=team)
 
     return st.plotly_chart(fig1)
 
 
+#weekly charts
+
+#filter for year week team and their oppenent stats for that matchup
+def matchup_week_stats(year: int, week: int, team: str, opponent: str):
+    file = 'Weekly Stats/Team_Weekly_Stats'
+
+    table_df = pd.read_csv(file, index_col=0)
+    
+    query = f'''
+        select *
+        from table_df
+        where (Year={year}) and (Week={week}) and (Team='{team}' or Team='{opponent}')
+    '''
+
+    df = duckdb.sql(query).df()
+    
+    return df
+
+#filter for team and opponent
+#to graph all season matchups
+def filter_matchup_history(team: str, opponent:str):
+    
+    file = f'Weekly Stats/Team_Weekly_Stats'
+
+    table_df = pd.read_csv(file, index_col=0)
+    
+    query = f'''
+        select *
+        from table_df
+        where (Team='{team}' or Team='{opponent}') and (Opponent='{team}' or Opponent='{opponent}')
+        order by Year, Team
+    '''
+
+    df = duckdb.sql(query).df()
+
+    return df
+
+
+#bar chart for weekly
+def weekly_px_bar_charts(df: pd, team, column):
+    fig1 = px.bar(
+        df, 
+        x=df['Team'], 
+        y=df[column], 
+        barmode='group',
+        color=df['Team'],
+        labels=df['Team'],
+        title=f'{team}'
+    )
+
+    return st.plotly_chart(fig1)
+
+
+#piechart for wekly
+def weekly_px_pie_charts(df, team, column):
+    fig1 = px.pie(data_frame=df, values=df[column], names=df['Team'], title=team)
+
+    return st.plotly_chart(fig1)
+
+                     
+
 ############ streamlit
 #set up multipage link
 st.set_page_config(page_title='Team', layout='wide', initial_sidebar_state='expanded')
 
 #Title
-st.title('Team Results')
+st.title('Player Results')
 
-#uer input dropdown boxes for year team and table
+#user input dropdown boxes for year team and table
 year_select = st.sidebar.selectbox('Select Year', options=years)
-team_select = st.sidebar.selectbox('Select Team 1', options=team_names)
-table_select = st.sidebar.selectbox('Select Table', options= table_title[:-3])
+team_select = st.sidebar.selectbox('Select Team', options=team_names)
 
 #get select week
 sched_df = team_schedule(year_select, team_select)
 week_select = st.sidebar.selectbox('Select Week', sched_df['Week'])
 
 
-#filter dataframe for team and year
-#oppoennt dataframe
-df, opponent = query_df(sched_df, year_select, team_select, tables[table_select], week_select)
+
+#opponent
+opponent = get_opponent(year_select, week_select, team_select)
 
 
-#filter datframe for year
-year_df = df_year(year_select, tables[table_select])
+
 
 
 ##################################################################      Main area
@@ -188,25 +262,93 @@ year_df = df_year(year_select, tables[table_select])
 #title
 st.title(f'NFL Data {year_select} {team_select} vs. {opponent}')
 
-#show dataframe
-st.write(team_select)
-st.dataframe(df[df['Team']==team_select], column_config={'index':None, 'Team':None, 'Year':None})
-st.write(opponent)
-st.dataframe(df[df['Team']==opponent], column_config={'index':None, 'Team':None, 'Year':None})
+
+#########expanders/ drop downs
+
+############### weekly match up
+with st.expander(f'{year_select}: Game Stats {team_select} vs. {opponent}'):
+    # team weekly stats
+    team_weekly_stats = matchup_week_stats(year_select, week_select, team_select, opponent)
+    
+    df_col = team_weekly_stats.columns[:-2]
+     
+    #select column
+    column_select = st.selectbox('Columns', df_col)
+
+    #Graph title
+    st.header(column_select.title())
+
+        #show dataframe
+    st.write(team_select)
+    st.dataframe(team_weekly_stats, column_config={'index':None, 'Team':None, 'Year':None})
+
+    
+    c1, c2 = st.columns((5,5))
+    with c1:
+        #barchart  
+        weekly_px_bar_charts(team_weekly_stats, team_select, column_select)
+    with c2:
+        weekly_px_pie_charts(team_weekly_stats, team_select, column_select)
 
 
-df_col = list(df.columns)
-try:
-    df_col.remove('Team')
-except:
-    pass
+########### match up history
+with st.expander(f'Matchup History: {team_select} vs. {opponent}'):
+
+    matchup_history = filter_matchup_history(team_select, opponent)
+
+    
+    df_col = matchup_history.columns
+     
+    #select column
+    column_select = st.selectbox('Columns', df_col)
+
+    #Graph title
+    st.header(column_select.title())
+
+        #show dataframe
+    st.write(team_select)
+    st.dataframe(matchup_history, column_config={'index':None, 'Team':None, 'Year':None})
+
+    
+    c1, c2 = st.columns((5,5))
+    with c1:
+        #barchart  
+        weekly_px_bar_charts(matchup_history, team_select, column_select)
+    with c2:
+        weekly_px_pie_charts(matchup_history, team_select, column_select)
 
 
-#expanders/ drop downs
-with st.expander(f'{year_select}: {team_select} vs. {opponent}'):
+############ team all year
+with st.expander(f'{team_select} vs. {opponent} Season {year_select} Comparison'):
+    
+    #select table
+    table_select = st.selectbox('Select Table', options= table_title[:-3])
+    
+    #filter dataframe for team and year
+    #oppoennt dataframe
+    df, opponent = opp_table_df(sched_df, year_select, team_select, tables[table_select], week_select)
+    #filter datframe for year
+    year_df = df_year(year_select, tables[table_select])
+    
+    #show dataframe
+    st.write(team_select)
+    st.dataframe(df[df['Team']==team_select], column_config={'index':None, 'Team':None, 'Year':None})
+    st.write(opponent)
+    st.dataframe(df[df['Team']==opponent], column_config={'index':None, 'Team':None, 'Year':None})
+
+    
+    df_col = list(df.columns)
+    try:
+        df_col.remove('Team')
+    except:
+        pass
+
     #select column
     column_select = st.selectbox('Columns', df_col[2:])
 
+
+
+    
     #Graph title
     st.header(column_select.title())
     
--- pages/2_Players.py
+++ pages/2_Players.py
@@ -1,5 +1,10 @@
 #pipreqs --force
 
+### 7/10/2024 when internet back on rerun weekly player webscrape inorder to include week column
+########################
+################################   ^^^^^^^  READ ^^^^^^^
+############################################################################################
+
 import pandas as pd
 
 import plotly.express as px
@@ -8,5 +13,134 @@ import duckdb
 
 import streamlit as st
 
+
+
+#years
+years = range(2010,2024)
+
+team_names=[
+     '49ers',
+     'Bears',
+     'Bengals',
+     'Broncos',
+     'Browns',
+     'Buccaneers',
+     'Bills',
+     'Cardinals',
+     'Chargers',
+     'Chiefs',
+     'Colts',
+     'Commanders',
+     'Cowboys',
+     'Dolphins',
+     'Eagles',
+     'Falcons',
+     'Giants',
+     'Jaguars',
+     'Jets',
+     'Lions',
+     'Packers',
+     'Panthers',
+     'Patriots',
+     'Raiders',
+     'Rams',
+     'Ravens',
+     'Saints',
+     'Seahawks',
+     'Steelers',
+     'Texans',
+     'Titans',
+     'Vikings'
+]
+
+positions_dict = {
+    'QB': 'Quarter Back',
+    'RB': 'Running Back',
+    'WR': 'Wide Receiver',
+    'TE': 'Tight End',
+    'K': 'Kicker',
+    'DST': 'Defense and Special Teams',
+    'DL': 'Defensive Lineman',
+    'LB': 'Line Backer',
+    'DB': 'Defensive Back'
+}
+
+position_choices = list(positions_dict.values())
+position_abbrev = list(positions_dict.keys())
+
+# position = position_abbrev
+def player_df (year, week, position, player):
+
+    file = f'Player Weekly Stats/{position}_Player_Weekly_Stats'
+
+    df = pd.read_csv(file, index_col=0)
+
+    query = f'''
+        select * 
+        from {position}_Player_Weekly_Stats
+        where (Week = {week}) 
+        and (Year = {year}) 
+        and (Player = {player})
+    '''
+
+    df = duckdb.sql(query).df()
+    
+    return df
+
+
+#bar chart
+def px_bar_charts(df: pd):
+    try:
+        fig1 = px.bar(
+            df, 
+            x= 
+            y=df[column], 
+            barmode='group',
+            color=df['Player'],
+            labels=df['Player'].iloc[:],
+            title=f'{team}'
+        )
+    
+    except:
+        fig1 = px.bar(
+            df, 
+            x=df[column].index, 
+            y=df[column],
+            barmode='group',
+            color=df[column].index,
+            labels=df['Player'].iloc[:],
+            title=team
+        )
+
+    return st.plotly_chart(fig1)
+
+
+#piechart
+def px_pie_charts(df, team, column):
+    fig1 = px.pie(data_frame=df, values=df[column], names=df['Player'], title=team)
+
+    return st.plotly_chart(fig1)
+
+
+
+######### Data Frames
+
+
+
+
+
+################### Players main st area
 st.set_page_config(page_title='Players', layout='wide', initial_sidebar_state='expanded')
 
+
+
+st.title('Player Results')
+
+#uer input dropdown boxes for year team and table
+year_select = st.sidebar.selectbox('Select Year', options=years)
+team_select = st.sidebar.selectbox('Select Team', options=team_names)
+table_select = st.sidebar.selectbox('Select Position', options=position_choices)
+
+#get select week
+sched_df = team_schedule(year_select, team_select)
+week_select = st.sidebar.selectbox('Select Week', sched_df['Week'])
